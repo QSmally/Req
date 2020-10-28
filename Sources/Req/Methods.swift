23c1c91226fbcaa5153c502df96f5dd8a9482409
QSmally <github@qbot.eu>
2020-10-28 14:45:54 +0100

    Implement Method enum

---
+++ Sources/Req/Methods.swift
@@ -0,0 +1,17 @@
+//
+//  File.swift
+//  
+//
+//  Created by Joey Smalen on 28/10/2020.
+//
+
+import Foundation
+
+public enum Method: String {
+    case GET,
+         POST,
+         PUT,
+         PATCH,
+         HEAD,
+         DELETE
+}
