a11e15566600920ea3cecefb65a011c4a489597e
QSmally <github@qbot.eu>
2020-11-04 10:59:55 +0100

    Type func and URL encoding

--- Sources/Req/Req.swift
+++ Sources/Req/Req.swift
@@ -6,22 +6,36 @@
 //  Copyright © 2020 Joey Smalen. All rights reserved.
 //
 
+import Foundation
+
 struct Req {
     
-    var url: String
+    var url: URL
     
     private var headers         = [String: String]()
     private var response        = [String: Any]()
     private var method: Method  = Method.GET
     
     
-    init(url: String) {
-        self.url = url
+    init(url: String) throws {
+        guard let baseUrl = URL(string: url) else {
+            throw NSException(
+                name: NSExceptionName.invalidUnarchiveOperationException,
+                reason: "URL cannot be resolves or address is invalid"
+            ) as! Error
+        }
+        
+        self.url = baseUrl
     }
     
     
     mutating func header(key: String, value: String) -> Self {
         self.headers[key] = value
+        return self
+    }
+    
+    mutating func type(method: Method) -> Self {
+        self.method = method
         return self
     }
 }
