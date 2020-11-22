6168418b78141e29e7307937bb07c7fbb06a298b
QSmally <github@qbot.eu>
2020-11-22 19:20:49 +0100

    Unit tests

--- Tests/ReqTests/ReqTests.swift
+++ Tests/ReqTests/ReqTests.swift
@@ -7,6 +7,13 @@ final class ReqTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
+        
+        var myReq = try? Req(url: "https://jsonplaceholder.typicode.com/todos")
+        
+        myReq?.header(key: "X-Headers-Work", value: "1")
+        myReq?.header(key: "Cache-Control", value: "nostore")
+        
+        myReq?.send();
     }
     
     func baseTest() {
